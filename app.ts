e779f93c2bfe63b739d6f126e9ec459b33b32c34
Git Modder <gitmodder@aol.com>
2022-09-10 02:15:50 +0500

    Use SendinBlue

--- app.ts
+++ app.ts
@@ -1,14 +1,24 @@
 import express from "express";
 import { json } from "body-parser";
 import { emailRouter } from "./mail_controller";
+import cors from 'cors';
 
+const corsOptions = {
+  allowedHeaders: 'Content-Type,Access-Control-Allow-Origin',
+  origin: '*',
+  exposedHeaders: 'Access-Control-Allow-Origin',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 const app = express();
+app.use(cors(corsOptions))
 app.use(json({ limit: "10mb" }));
 app.use(emailRouter);
-
+const PORT = process.env.PORT || 5000
 const start = async () => {
-  app.listen(process.env.PORT || 5000, () => {
-    console.log(`Listening on port ${process.env.PORT}.`);
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
   });
 };
 
--- backupcode.ts
+++ backupcode.ts
@@ -4,6 +4,20 @@ import { google } from "googleapis";
 import express, { Request, Response } from "express";
 const router = express.Router();
 
+const CLIENT_ID =
+  "467488087687-i8tbtqficd6ipgj5uqkajo9iovrhd0l9.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URL = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN =
+  "1//04hQzD0F1wtbBCgYIARAAGAQSNwF-L9IrRCM_Hc5xnY18Ps7vjgeaR6WdTcdl_Z2oc3vt0rxlTfKoKHQilrY0ynbE4-ES8hlyjto";
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL
+);
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
 router.get("/", (req: Request, res: Response) => {
   res.status(200).json("Server is up and running.");
 });
@@ -19,17 +33,20 @@ router.post("/send-email", async (req: Request, res: Response) => {
 
 const sendMail = async (toEmail: string) => {
   try {
+    const accessToken = oAuth2Client.getAccessToken();
     const mailer = nodemailer.createTransport({
-      host: "smtp-relay.sendinblue.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
+      service: "gmail",
       auth: {
-        user: 'bottwelve@inbox.lv', // generated ethereal user
-        pass: 'BDKCLdVOgPvAItwf', // generated ethereal password
+        type: "OAuth2",
+        user: "talhamuneerearning@gmail.com",
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken,
       },
     });
     const mailOptions = {
-      from: "bottwelve@inbox.lv",
+      from: "talhamuneerearning@gmail.com",
       to: toEmail,
       subject: `Get In Touch ${toEmail}`,
       text: "The Details I Will Get Here.",
@@ -41,5 +58,3 @@ const sendMail = async (toEmail: string) => {
     return error;
   }
 };
-
-export { router as emailRouter };
